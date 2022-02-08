01fc2368486c05bfe97a5c217a0e000b26da2436
tfqqrman <tfqqrman@gmail.com>
2022-02-08 15:05:08 +0700

    add menu

--- app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.recyclerviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -18,7 +20,30 @@ class MainActivity : AppCompatActivity() {
 
         list.addAll(HeroesData.listData)
         showRecyclerList()
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int){
+        when(selectedMode){
+            R.id.action_list -> {
+            }
+            R.id.action_grid -> {
+            }
+            R.id.action_cardview ->{
+            }
+        }
+    }
+
     private fun showRecyclerList(){
         rvHeroes.layoutManager = LinearLayoutManager(this)
         val listHeroAdapter = ListHeroAdapter(list)
